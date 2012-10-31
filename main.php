98fbe05523be8629735754a062b76cf957df784d
Nickolay Ribal <elektronik2k5@gmail.com>
2012-10-31 23:53:40 +0200

    Filesystem representation creation mechanism working

--- main.php
+++ main.php
@@ -31,21 +31,13 @@ class VirtualDirectory implements iNode {
 	public function __construct($name){
 		$this->name = $name;
     }
-    
     protected function getName(){
     	return $this->name;
     }
-	
 	public function addChild($childNode){
-		$childName = $childNode->toString();
-		if (array_key_exists($childName, $this->children)) {
-			throw new Exception("Can't add child with name '$childName', since this directory already has a child with this name", 1);
-		} else {
-			$this->children[] = $childNode;
-		}
-		return true;
+		$this->children[] = $childNode;
+		return $childNode;
 	}
-
 	public function toString(){
     	print $this->getName()."\n";
     	foreach ($this->children as $childNode) {
@@ -55,16 +47,19 @@ class VirtualDirectory implements iNode {
     }
 }
 
-class Program {
+class VirtualDirectoryCreator {
 	public static $padding = "    ";
-	public static function readDir($directory, $padding = '') {
+	public static function readDir($node, $directory, $padding = '') {
 		static $ownFunctionName = __FUNCTION__;
 		if ($handle = @opendir($directory)) {
 			while (($entry = readdir($handle)) !== false) {
 				if ($entry != "." && $entry != "..") {
 					echo "${padding}└── $entry\n";
-					if (is_dir($childDir = $directory.DIRECTORY_SEPARATOR.$entry)) {
-						self::$ownFunctionName($childDir, $padding.self::$padding);
+					$isChildDirectory = is_dir($childDirectory = $directory.DIRECTORY_SEPARATOR.$entry);
+					$newNode = $isChildDirectory ? (new VirtualDirectory($entry)) : (new VirtualFile($entry));
+					$node->addChild($newNode);
+					if ($isChildDirectory) {
+						self::$ownFunctionName($newNode, $childDirectory, $padding.self::$padding);
 					}
 				}
 			}
@@ -72,8 +67,17 @@ class Program {
 		} else {
 			error("Cannot read '$directory' directory. Could be a permissions or transport error. Exiting.") || die();
 		}
+		return $node;
 	}
 
+	private $root;
+	public function __construct($path){
+		$root = new VirtualDirectory($path);
+		$this->root = self::readDir($root, $path);
+	}
+}
+
+class Program {
 	public static function main($args){
 		if (count($args) != 2) {
 			error("Usage: ".$args[0]." PATH_TO_DIRECTORY") || die();
@@ -83,30 +87,9 @@ class Program {
 			error("'$givenPath' isn't a directory. Exiting.") || die();
 		}
 		p($givenPath);
-		self::readDir($givenPath);
+		$vd = new VirtualDirectoryCreator($givenPath);
+		print_r($vd);
 	}
 }
-
-class VirtualDirectoryCreator {
-	private $root;
-	public function __construct($args){
-		
-	}
-}
-
-
 
 Program::main($argv);
-return;
-$f1 = new VirtualFile('1');
-$f2 = new VirtualFile('second');
-$d1 = new VirtualDirectory('root');
-$d1->addChild($f1);
-
-$files = array($f1, $f2, $d1);
-foreach ($files as $file) {
-	p();
-	print $file->toString();
-}
-
-p();
