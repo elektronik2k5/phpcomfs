604635aed07a4e30a5012f8bb09d69ce144e89ce
Nickolay Ribal <nick.r@grimfell.office.taboola.com>
2012-11-01 16:12:30 +0200

    Added filesystem objects' sizes

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?php
+
 function p($message = ''){
 	print($message."\n");
 }
@@ -9,71 +10,90 @@ function error($message){
 
 interface iNode {
 	public function toString();
+	public function getSize();
+	public function getChildren();
+}
+
+function human_filesize($bytes, $decimals = 2) {
+	$sz = 'BKMGTP';
+	$factor = floor((strlen($bytes) - 1) / 3);
+	return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
 }
 
 class VirtualFile implements iNode {
-	private $name;
+	private $name, $size;
 	public function __construct($name){
-		$this->name = $name;
-    }
-    protected function getName(){
-    	return $this->name;
-    }
-    public function toString(){
-    	return $this->getName()."\n";
-    }
+		$this->name = basename($name);
+		$this->size = filesize($name);
+	}
+	protected function getName(){
+		return $this->name;
+	}
+	public function getSize(){
+		return $this->size;
+	}
+	public function getChildren(){
+		return null;
+	}
+	public function toString(){
+		return sprintf("%s [%s]", $this->getName(), human_filesize($this->getSize(), 0));
+	}
 }
 
 class VirtualDirectory implements iNode {
-	private $children = array();
-	private $name;
+	private $name, $size = null, $children = array();
 	
-	public function __construct($name){
-		$this->name = $name;
-    }
-    protected function getName(){
-    	return $this->name;
-    }
-	public function addChild($childNode){
-		$this->children[] = $childNode;
-		return $childNode;
-	}
-	public function toString(){
-    	print $this->getName()."\n";
-    	foreach ($this->children as $childNode) {
-    		$childNode->toString();
-    	}
-    	return true;
-    }
-}
-
-class VirtualDirectoryCreator {
-	public static $padding = "    ";
-	public static function readDir($node, $directory, $padding = '') {
-		static $ownFunctionName = __FUNCTION__;
-		if ($handle = @opendir($directory)) {
+	public function __construct($directory){
+		if ($handle = opendir($directory)) {
+			$this->name = basename($directory);
 			while (($entry = readdir($handle)) !== false) {
 				if ($entry != "." && $entry != "..") {
-					echo "${padding}└── $entry\n";
-					$isChildDirectory = is_dir($childDirectory = $directory.DIRECTORY_SEPARATOR.$entry);
-					$newNode = $isChildDirectory ? (new VirtualDirectory($entry)) : (new VirtualFile($entry));
-					$node->addChild($newNode);
-					if ($isChildDirectory) {
-						self::$ownFunctionName($newNode, $childDirectory, $padding.self::$padding);
-					}
+					//echo "└── $entry\n";
+					$childNode = $directory.DIRECTORY_SEPARATOR.$entry;
+					$isChildDirectory = is_dir($childNode);
+					$vd = get_class($this);
+					$newNode = $isChildDirectory ? (new $vd($childNode)) : (new VirtualFile($childNode));
+					$this->children[] = $newNode;
 				}
 			}
 			closedir($handle);
 		} else {
 			error("Cannot read '$directory' directory. Could be a permissions or transport error. Exiting.") || die();
 		}
-		return $node;
 	}
+	
+	protected function getName(){
+		return $this->name;
+	}
+	public function getSize(){
+		if ($this->size !== null) {
+			return $this->size;
+		}
+		$size = 0;
+		foreach ($this->children as $childNode) {
+			$size += $childNode->getSize();
+		}
+		return $this->size = $size;
+	}
+	public function getChildren(){
+		return $this->children;
+	}
+	public function toString(){
+		$childNodes = '';
+		foreach ($this->children as $childNode) {
+			$childNodes .= "└── ".$childNode->toString()."\n";
+		}
+		return sprintf("%s [%s]\n%s", $this->getName(), human_filesize($this->getSize(), 0), $childNodes);
+	}
+}
 
+class VirtualFileSystem {
 	private $root;
 	public function __construct($path){
-		$root = new VirtualDirectory($path);
-		$this->root = self::readDir($root, $path);
+		$this->root = new VirtualDirectory($path);
+	}
+	public function walkFs(){
+		print $this->root->toString();
 	}
 }
 
@@ -87,8 +107,11 @@ class Program {
 			error("'$givenPath' isn't a directory. Exiting.") || die();
 		}
 		p($givenPath);
-		$vd = new VirtualDirectoryCreator($givenPath);
-		print_r($vd);
+		//$vd = new VirtualDirectory($givenPath);
+		//print_r($vd);
+		//print $vd->toString();
+		$vfs = new VirtualFileSystem($givenPath);
+		$vfs->walkFs();
 	}
 }
 
