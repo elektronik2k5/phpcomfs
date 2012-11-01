8ed083a5f7a75c84b8b3c626451e3cbf0b9ef7c7
Nickolay Ribal <elektronik2k5@gmail.com>
2012-11-02 00:14:37 +0200

    Almost done

--- main.php
+++ main.php
@@ -1,23 +1,14 @@
 #!/usr/bin/php
 <?php
 
-function p($message = ''){
-	print($message."\n");
-}
 function error($message){
 	file_put_contents('php://stderr', "$message\n");
 }
 
 interface iNode {
-	public function toString();
+	public function getName();
 	public function getSize();
 	public function getChildren();
-}
-
-function human_filesize($bytes, $decimals = 2) {
-	$sz = 'BKMGTP';
-	$factor = floor((strlen($bytes) - 1) / 3);
-	return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
 }
 
 class VirtualFile implements iNode {
@@ -26,7 +17,7 @@ class VirtualFile implements iNode {
 		$this->name = basename($name);
 		$this->size = filesize($name);
 	}
-	protected function getName(){
+	public function getName(){
 		return $this->name;
 	}
 	public function getSize(){
@@ -35,13 +26,10 @@ class VirtualFile implements iNode {
 	public function getChildren(){
 		return null;
 	}
-	public function toString(){
-		return sprintf("%s [%s]", $this->getName(), human_filesize($this->getSize(), 0));
-	}
 }
 
 class VirtualDirectory implements iNode {
-	private $name, $size = null, $children = array();
+	private $name, $children = array();
 	
 	public function __construct($directory){
 		if ($handle = opendir($directory)) {
@@ -62,38 +50,31 @@ class VirtualDirectory implements iNode {
 		}
 	}
 	
-	protected function getName(){
+	public function getName(){
 		return $this->name;
 	}
 	public function getSize(){
-		if ($this->size !== null) {
-			return $this->size;
-		}
-		$size = 0;
-		foreach ($this->children as $childNode) {
-			$size += $childNode->getSize();
-		}
-		return $this->size = $size;
+		return null;
 	}
 	public function getChildren(){
 		return $this->children;
 	}
-	public function toString(){
-		$childNodes = '';
-		foreach ($this->children as $childNode) {
-			$childNodes .= "└── ".$childNode->toString()."\n";
-		}
-		return sprintf("%s [%s]\n%s", $this->getName(), human_filesize($this->getSize(), 0), $childNodes);
-	}
 }
 
 class VirtualFileSystem {
-	private $root;
-	public function __construct($path){
-		$this->root = new VirtualDirectory($path);
+	public static function walkFs($node, $callback){
+		$callback($node);
+		static $ownName = __FUNCTION__;
+		if (($childNodes = $node->getChildren()) !== null) {
+			foreach ($childNodes as $childNode) {
+				self::$ownName($childNode, $callback);
+			}
+		}
 	}
-	public function walkFs(){
-		print $this->root->toString();
+	public static function getHumanFileSize($bytes, $decimals = 2) {
+		$sz = 'BKMGTP';
+		$factor = floor((strlen($bytes) - 1) / 3);
+		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
 	}
 }
 
@@ -106,12 +87,30 @@ class Program {
 		if (!is_dir($givenPath)) {
 			error("'$givenPath' isn't a directory. Exiting.") || die();
 		}
-		p($givenPath);
-		//$vd = new VirtualDirectory($givenPath);
-		//print_r($vd);
-		//print $vd->toString();
-		$vfs = new VirtualFileSystem($givenPath);
-		$vfs->walkFs();
+		$vfs = 'VirtualFileSystem';
+		$printNodes = function($node){
+			printf("\t%s\n", $node->getName());
+		};
+		$printDirectories = function($node) use ($vfs, $printNodes){
+			if ($node->getChildren() === null) {
+				return;
+			}
+			return $printNodes($node);
+		};
+		$printNodesAndSizes = function($node) use ($vfs){
+			$nodeSize = $node->getSize();
+			$humanSize = array($vfs, 'getHumanFileSize');
+			$sizeToPrint = $nodeSize === null ? 'DIR' : call_user_func($humanSize, $nodeSize, 0);
+			printf("\t%s [%s]\n", $node->getName(), $sizeToPrint);
+		};
+		echo "Building data structure of directory '$givenPath'...\n";
+		$root = new VirtualDirectory($givenPath);
+		echo "\nDirectory recursive structure:\n";
+		$vfs::walkFs($root, $printNodes);
+		echo "\nDirectory recursive structure, only directories:\n";
+		$vfs::walkFs($root, $printDirectories);
+		echo "\nDirectory recursive structure, with file sizes:\n";
+		$vfs::walkFs($root, $printNodesAndSizes);
 	}
 }
 
